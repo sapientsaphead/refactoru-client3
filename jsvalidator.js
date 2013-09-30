a381c7a8d90a241606a87237213f8f8c5872fad2
Debra Zapata <debra.zapata@gmail.com>
2013-09-30 16:37:42 -0600

    Resolved Issues

--- jsvalidator.js
+++ jsvalidator.js
@@ -1,7 +1,7 @@
 
 //Phone Number
 var phoneno = prompt("Please enter your phone number with dashes. Rest assured we will not spam you.","");
-if(phoneno.charAt(3) != "-" || phoneno.charAt(7) != "-"){
+if(phoneno.charAt(3) !== "-" || phoneno.charAt(7) !== "-" || phoneno.length !== 12){
 	prompt("You must enter a valid phone number.");
 }
 else {
@@ -10,7 +10,7 @@ else {
 
 //Birth Date
 var birthdate = prompt("Please enter your birth date.","MM/DD/YY");
-if(birthdate.charAt(2) != "/" || birthdate.charAt(5) != "/"){
+if(birthdate.charAt(2) !== "/" || birthdate.charAt(5) !== "/"){
 	prompt("You must enter a valid birth date.");
 }
 else {
